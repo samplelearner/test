5c54bdfff0e4e9917938a968346c64c8f4f5b8af
sreehari.v <sreehari.vv007@gmail.com>
2015-06-10 14:49:52 +0530

    msg

---
+++ src/gitstudy.java
@@ -0,0 +1,26 @@
+/**
+ * Created by sreehari.v on 10/06/15.
+ */
+public class gitstudy {
+
+    public void learn()
+    {
+        System.out.println("learning git commits");
+        System.out.println("automatic commits");
+        int a=10;
+        int b=20;
+        int c= a+b;
+        System.out.println(c);
+
+    }
+
+    public void secondlearn()
+    {
+
+        learn();
+        System.out.println("check check");
+        System.out.println("forwarding to git");
+
+    }
+
+}
